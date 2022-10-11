fb4e062144b202744c94f21ffde4a4d827a21862
Array <shellmode13@gmail.com>
2022-10-11 16:08:08 +0800

    athena query group by interface-id

--- Optimize-Costs-For-NAT/aws-service-to-nat.sql
+++ Optimize-Costs-For-NAT/aws-service-to-nat.sql
@@ -1,5 +1,5 @@
-SELECT "pkt-src-aws-service", SUM("bytes") as DataTransferred
+SELECT "interface-id", "pkt-src-aws-service", SUM("bytes") as DataTransferred
 FROM "vpcflowlogdb"."vpcflowlogtable"
 WHERE "pkt-src-aws-service" != '-'
-GROUP BY "pkt-src-aws-service"
+GROUP BY "pkt-src-aws-service", "interface-id"
 ORDER BY "DataTransferred" DESC
--- Optimize-Costs-For-NAT/nat-to-aws-service.sql
+++ Optimize-Costs-For-NAT/nat-to-aws-service.sql
@@ -1,5 +1,5 @@
-SELECT "pkt-dst-aws-service", SUM("bytes") as DataTransferred
+SELECT "interface-id", "pkt-dst-aws-service", SUM("bytes") as DataTransferred
 FROM "vpcflowlogdb"."vpcflowlogtable"
 WHERE "pkt-dst-aws-service" != '-'
-GROUP BY "pkt-dst-aws-service"
+GROUP BY "pkt-dst-aws-service", "interface-id"
 ORDER BY "DataTransferred" DESC
